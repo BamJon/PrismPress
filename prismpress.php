67492b9e9d6a2cef8f1791e413e6f55ccc049771
BamJon <97885341+BamJon@users.noreply.github.com>
2025-01-29 11:53:33 +0000

    Update prismpress.php

--- prismpress.php
+++ prismpress.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * Version:           0.1.0
- * Author:            Jon  
+ * Author:            Jon  B
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       prismpress
