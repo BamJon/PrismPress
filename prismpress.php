a1fde54c05711bcbb2ac4e726f519e8c481823f0
BamJon <97885341+BamJon@users.noreply.github.com>
2025-01-29 11:34:51 +0000

    Update prismpress.php

--- prismpress.php
+++ prismpress.php
@@ -4,7 +4,7 @@
  * Description:       A Gutenberg code block utilising Prism.js
  * Requires at least: 6.0
  * Requires PHP:      8.0
- * Version:           0.0.5
+ * Version:           0.0.6
  * Author:            Jon  
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
